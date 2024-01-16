4216ddf783b92c710a709f472a7a75220d6ee7e6
jordan-pg <jordan.p.griffin@gmail.com>
2024-01-16 13:34:56 -0800

    path resolve

--- lib/templateEngine.ts
+++ lib/templateEngine.ts
@@ -4,7 +4,7 @@ import Handlebars from 'handlebars';
 import * as path from 'path';
 
 export async function populateTemplate(data: Resume, type: string): Promise<string> {
-    const newPath = path.resolve(`resume-builder-api/templates/${type}.hbs`);
+    const newPath = path.resolve(`../../resume-builder-api/templates/${type}.hbs`);
 
     const templateSource = await fs.readFile(newPath, 'utf8');
     const template = Handlebars.compile(templateSource);
