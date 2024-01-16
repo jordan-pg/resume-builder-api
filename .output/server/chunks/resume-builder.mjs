847d98a067ee57c5f9946a3c840b4749ec8ce0ab
jordan-pg <jordan.p.griffin@gmail.com>
2024-01-15 23:23:34 -0800

    nitro config and build

---
+++ .output/server/chunks/resume-builder.mjs
@@ -0,0 +1,57 @@
+import { d as defineEventHandler, h as handleCors, a as assertMethod, r as readBody, c as createError } from './nitro/node-server.mjs';
+import puppeteer from 'puppeteer';
+import * as fs from 'fs';
+import Handlebars from 'handlebars';
+import 'node:http';
+import 'node:https';
+import 'path';
+import 'node:fs';
+import 'node:url';
+
+async function generatePDF(htmlContent) {
+  const browser = await puppeteer.launch({
+    defaultViewport: null,
+    executablePath: "/Applications/Google Chrome.app/Contents/MacOS/Google Chrome",
+    headless: "new"
+  });
+  const page = await browser.newPage();
+  await page.setContent(htmlContent, { waitUntil: "networkidle0" });
+  const pdfBuffer = await page.pdf({ format: "a4", printBackground: true });
+  await browser.close();
+  return pdfBuffer;
+}
+
+function populateTemplate(data, type) {
+  const templateSource = fs.readFileSync(`templates/${type}.hbs`, "utf8");
+  const template = Handlebars.compile(templateSource);
+  return template(data);
+}
+
+const resumeBuilder = defineEventHandler(async (event) => {
+  try {
+    if (event.method === "OPTIONS") {
+      handleCors(event, {});
+      return null;
+    }
+    assertMethod(event, ["POST"]);
+    const body = await readBody(event);
+    const parsedBody = JSON.parse(body);
+    const htmlContent = populateTemplate(parsedBody.data, parsedBody.type);
+    const pdfBuffer = await generatePDF(htmlContent);
+    return new Response(pdfBuffer, {
+      headers: {
+        "Content-Type": "application/pdf",
+        "Content-Disposition": 'attachment; filename="resume.pdf"'
+      }
+    });
+  } catch (error) {
+    console.error(error);
+    return createError({
+      statusCode: 500,
+      statusMessage: error
+    });
+  }
+});
+
+export { resumeBuilder as default };
+//# sourceMappingURL=resume-builder.mjs.map
--- nitro.config.ts
+++ nitro.config.ts
@@ -1,2 +1,9 @@
 //https://nitro.unjs.io/config
-export default defineNitroConfig({});
+export default defineNitroConfig({
+    routeRules: {
+        "/*": {
+          cors: true,
+          headers: { "access-control-allow-methods": "POST" },
+        },
+      },
+});
