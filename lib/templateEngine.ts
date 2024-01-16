1116441fe708ba5abef1dc059d7351062558a8c7
jordan-pg <jordan.p.griffin@gmail.com>
2024-01-16 13:48:40 -0800

    public

--- lib/templateEngine.ts
+++ lib/templateEngine.ts
@@ -1,9 +1,11 @@
 import { Resume } from '../types/resumeTypes';
 import * as fs from 'fs';
 import Handlebars from 'handlebars';
+import path from 'path'
 
 export async function populateTemplate(data: Resume, type: string): Promise<string> {
-    const templateSource = fs.readFileSync(`public/${type}.hbs`, 'utf8');
-    const template = Handlebars.compile(templateSource);
+    const pathResolve = await path.resolve(process.cwd(), `/public/${type}.hbs`)
+    const templateSource = await fs.readFileSync(pathResolve, 'utf8');
+    const template = await Handlebars.compile(templateSource);
     return template(data);
 }
