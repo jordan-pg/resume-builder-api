b57729390555f71acc235defe3855125ce48c0cc
jordan-pg <jordan.p.griffin@gmail.com>
2024-01-16 13:32:12 -0800

    path resolve

--- lib/templateEngine.ts
+++ lib/templateEngine.ts
@@ -4,7 +4,7 @@ import Handlebars from 'handlebars';
 import * as path from 'path';
 
 export async function populateTemplate(data: Resume, type: string): Promise<string> {
-    const newPath = path.resolve(`../templates/${type}.hbs`);
+    const newPath = path.resolve(`../../templates/${type}.hbs`);
 
     const templateSource = await fs.readFile(newPath, 'utf8');
     const template = Handlebars.compile(templateSource);
