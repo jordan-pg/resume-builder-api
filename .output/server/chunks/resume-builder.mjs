2e8d6c37824243e782fa48833fb186a1e05a9f0f
jordan-pg <jordan.p.griffin@gmail.com>
2024-01-16 14:09:56 -0800

    revert

---
+++ .output/server/chunks/resume-builder.mjs
@@ -0,0 +1,60 @@
+import { d as defineEventHandler, h as handleCors, a as assertMethod, r as readBody, c as createError } from './nitro/node-server.mjs';
+import puppeteer from 'puppeteer';
+import * as fs from 'fs';
+import Handlebars from 'handlebars';
+import path from 'path';
+import 'node:http';
+import 'node:https';
+import 'node:fs';
+import 'node:url';
+
+async function generatePDF(htmlContent) {
+  const browser = await puppeteer.launch({
+    defaultViewport: null,
+    executablePath: "/Applications/Google Chrome.app/Contents/MacOS/Google Chrome",
+    headless: "new"
+  });
+  const page = await browser.newPage();
+  await page.setContent(htmlContent, { waitUntil: "networkidle0" });
+  const pdfBuffer = await page.pdf({ format: "a4", printBackground: true });
+  await browser.close();
+  return pdfBuffer;
+}
+
+async function populateTemplate(data, type) {
+  const pathResolve = await path.resolve(`/.vercel/output/static/${type}.hbs`);
+  const templateSource = await fs.readFileSync(pathResolve, "utf8");
+  const template = await Handlebars.compile(templateSource);
+  return template(data);
+}
+
+const resumeBuilder = defineEventHandler(async (event) => {
+  try {
+    if (event.method === "OPTIONS") {
+      handleCors(event, {});
+      return null;
+    }
+    assertMethod(event, ["POST"]);
+    const body = await readBody(event);
+    const parsedBody = JSON.parse(body);
+    const htmlContent = await populateTemplate(parsedBody.data, parsedBody.type);
+    const pdfBuffer = await generatePDF(htmlContent);
+    return new Response(pdfBuffer, {
+      headers: {
+        "Content-Type": "application/pdf",
+        "Content-Disposition": 'attachment; filename="resume.pdf"'
+      }
+    });
+  } catch (error) {
+    const stackTrace = new Error().stack;
+    console.error(error);
+    return createError({
+      statusCode: 500,
+      statusMessage: error,
+      stack: stackTrace
+    });
+  }
+});
+
+export { resumeBuilder as default };
+//# sourceMappingURL=resume-builder.mjs.map
--- lib/templateEngine.ts
+++ lib/templateEngine.ts
@@ -1,11 +1,9 @@
 import { Resume } from '../types/resumeTypes';
 import * as fs from 'fs';
 import Handlebars from 'handlebars';
-import path from 'path'
 
 export async function populateTemplate(data: Resume, type: string): Promise<string> {
-    const pathResolve = await path.resolve(`/.vercel/output/static/${type}.hbs`)
-    const templateSource = await fs.readFileSync(pathResolve, 'utf8');
-    const template = await Handlebars.compile(templateSource);
+    const templateSource = fs.readFileSync(process.cwd() + `/public/${type}.hbs`, 'utf8');
+    const template = Handlebars.compile(templateSource);
     return template(data);
 }
