4e5d003f214a1e56e1e0968ff336a04bbaf5dc5f
jordan-pg <jordan.p.griffin@gmail.com>
2024-01-16 14:00:16 -0800

    point to output

--- lib/templateEngine.ts
+++ lib/templateEngine.ts
@@ -4,7 +4,7 @@ import Handlebars from 'handlebars';
 import path from 'path'
 
 export async function populateTemplate(data: Resume, type: string): Promise<string> {
-    const pathResolve = await path.resolve(process.cwd(), `public/${type}.hbs`)
+    const pathResolve = await path.resolve(`.output/public/${type}.hbs`)
     const templateSource = await fs.readFileSync(pathResolve, 'utf8');
     const template = await Handlebars.compile(templateSource);
     return template(data);
