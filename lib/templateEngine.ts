9e1aaddf6626b57629270fac66329ad58c06301e
jordan-pg <jordan.p.griffin@gmail.com>
2024-01-16 13:18:57 -0800

    trying templates

--- lib/templateEngine.ts
+++ lib/templateEngine.ts
@@ -3,7 +3,7 @@ import { promises as fs } from 'fs';
 import Handlebars from 'handlebars';
 
 export async function populateTemplate(data: Resume, type: string): Promise<string> {
-    const templateSource = await fs.readFile(`./templates/${type}.hbs`, 'utf8');
+    const templateSource = await fs.readFile(process.cwd() + `templates/${type}.hbs`, 'utf8');
     const template = Handlebars.compile(templateSource);
     return template(data);
 }
