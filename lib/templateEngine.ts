790cc0b05295e15bbd8a6ede89de07166896c997
jordan-pg <jordan.p.griffin@gmail.com>
2024-01-16 14:34:31 -0800

    config vercel with dir

--- lib/templateEngine.ts
+++ lib/templateEngine.ts
@@ -1,9 +1,13 @@
 import { Resume } from '../types/resumeTypes';
 import * as fs from 'fs';
 import Handlebars from 'handlebars';
+import path from 'path';
+import { fileURLToPath } from 'url';
 
 export async function populateTemplate(data: Resume, type: string): Promise<string> {
-    const templateSource = fs.readFileSync(process.cwd() +`/templates/${type}.hbs`, 'utf8');
+    const __dirname = fileURLToPath(new URL('.', import.meta.url));
+    const templatePath = path.join(__dirname, 'templates', `${type}.hbs`);
+    const templateSource = fs.readFileSync(templatePath, 'utf8');
     const template = Handlebars.compile(templateSource);
     return template(data);
 }
