2da7343c27c608df0738d38efa6ea925177a6715
jordan-pg <jordan.p.griffin@gmail.com>
2024-01-17 22:30:31 -0800

    require instead

--- lib/templateEngine.ts
+++ lib/templateEngine.ts
@@ -1,6 +1,6 @@
 import { Resume } from '../types/resumeTypes';
-import * as fs from 'fs';
-import Handlebars from 'handlebars';
+const fs = require("fs");
+const Handlebars = require("handlebars");
 
 export async function populateTemplate(data: Resume, type: string): Promise<string> {
     const templateSource = fs.readFileSync(process.cwd() +`/templates/${type}.hbs`, 'utf8');
